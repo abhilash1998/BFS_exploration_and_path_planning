26f8a8df6bb82ec2bbd139b06a0687ee3252c691
Abhilash Mane <37464530+abhilash1998@users.noreply.github.com>
2021-03-07 23:23:29 -0500

    Creating and initializing class

--- exploration_r.py
+++ exploration_r.py
@@ -1,2 +1,36 @@
 import numpy as np
 import cv2
+class exploration_r:
+    def __init__(self,start,goal):
+        """
+        Intializes variables for the class. Stores the goal state and
+        start state
+
+
+        Parameters
+        ----------
+        start : list
+            Starting point coordinates
+        goal : list
+            Goal point coordinates
+
+        """
+        self.ground_truth={}
+
+        self.obstacle=[]
+
+        self.expanded=[]
+
+        self.parent=[]
+        self.parent_orignal_data={}
+
+        self.start=start
+        self.frontier=[self.start]
+
+        self.frontier_string=[]
+
+        self.cost=0
+        self.goal=goal
+        self.data_with_string={}
+
+        self.current_score="00"
